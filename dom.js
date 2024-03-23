60d80bc4b893c1c5cdbb1657deeb68200d826e74
SumairaaNasir <163673362+SumairaaNasir@users.noreply.github.com>
2024-03-23 10:14:39 +0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+
+function update() {
+    console.log("Updating results");
+    var input = document.getElementById('myinput');
+    var output = document.getElementById('output'); 
+    //console.log(input.value);
+    output.innerHTML = input.value;
+}
